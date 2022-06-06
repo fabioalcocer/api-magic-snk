a733c528cf84e9586d29c54e2b65723196aac266
fabio alcocer <fabioalcocersejas17@gmail.com>
2022-06-06 18:47:04 -0400

    add media querie hover

--- js/reveals-images.js
+++
@@ -1,8 +0,0 @@
-export const revealImages = async () => {
-  setTimeout(() => {
-    const image = document.querySelectorAll('.char-img')
-    image.forEach((img) => {
-      img.style.opacity = '1'
-    })
-  }, 20000);
-}
